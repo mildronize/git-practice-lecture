5c9ef92d5f84b86b403802a042dcf742f978e005
Thada Wangthammang <mildronize@gmail.com>
2016-05-29 15:28:13 +0700

    Add main function

--- simple-calc.c
+++ simple-calc.c
@@ -1,1 +1,23 @@
-#include<stuio.h>
+#include<stdio.h>
+
+int main(){
+    int a,b,op;
+    int result=0;
+    printf("Enter an expression: ");
+    scanf("%d%c%d", &a, &b, &op);
+
+    if(op == '+')
+        result = plus(a,b);
+    else if(op == '-')
+        result = minus(a,b);
+    else if(op == '*')
+        result = multiply(a,b);
+    else if(op == '/')
+        result = divide(a,b);
+    else {
+        printf("Error");
+        return 1;
+    }
+    printf("The output is %d", result);
+    return 0;
+}
